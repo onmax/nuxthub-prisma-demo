d3fb5cade8fdf16bf8c08d6a78a85e7ac16f2566
onmax <maximogarciamtnez@gmail.com>
2026-01-20 13:24:01 +0100

    fix: add cloudflare runtime and wasm config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 export default defineNuxtConfig({
   modules: ['@nuxthub/core'],
   hub: { db: { orm: 'prisma', dialect: 'sqlite' } },
-  nitro: { preset: 'cloudflare-module' },
+  nitro: {
+    preset: 'cloudflare-module',
+    experimental: { wasm: true },
+  },
   future: { compatibilityVersion: 4 },
   compatibilityDate: '2025-01-01',
   devtools: { enabled: true },
