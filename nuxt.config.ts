4886adc38f8ddcbea1e0d028213fbf0a331f3bd1
onmax <maximogarciamtnez@gmail.com>
2026-01-20 13:03:53 +0100

    feat: add cloudflare-module preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 export default defineNuxtConfig({
   modules: ['@nuxthub/core'],
   hub: { db: { orm: 'prisma', dialect: 'sqlite' } },
+  nitro: { preset: 'cloudflare-module' },
   future: { compatibilityVersion: 4 },
   compatibilityDate: '2025-01-01',
   devtools: { enabled: true },
